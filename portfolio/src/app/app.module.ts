6e57490648402769575113d42847607d5011cf08
belleswin <80442734+belleswin@users.noreply.github.com>
2022-12-15 12:43:08 +1100

    styling

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
+import { SocialComponent } from './social/social.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AboutComponent,
-    ProjectComponent
+    ProjectComponent,
+    HomeComponent,
+    SocialComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
